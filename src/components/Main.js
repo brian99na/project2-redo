64b364fe782583c516640f3d0cabd3991b927110
brian99na <brian99na@gmail.com>
2021-11-22 23:00:09 -0800

    changed react router to react router dom

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Route } from "react-router";
+import { Route } from "react-router-dom";
 import Homepage from "./Homepage";
 import CryptoPage from './CryptoPage';
 import InflationPage from './InflationPage';
